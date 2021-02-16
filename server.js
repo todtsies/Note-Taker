4a87f446fb611b5dcb39cb9dc5b5fd66fa05c9e9
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-02-16 15:10:18 -0500

    Create server

--- server.js
+++ server.js
@@ -3,3 +3,19 @@ var express = require("express");
 var path = require("path");
 var fs = require('fs');
 var util = require('util');
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "./Develop/public")));
+
+//Set variables
+const writefileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+let allNotes;
