6e2c5e86027e9046468f3b9e36ebe35b41571487
Emily Todt <72036110+todtsies@users.noreply.github.com>
2021-02-16 19:03:35 -0500

    Update PORT
    Was unable to deploy to Heroku, trying to fix the problem

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8081;
 const mainDir = path.join(__dirname, "/public");
 
 app.use(express.static('public'));
